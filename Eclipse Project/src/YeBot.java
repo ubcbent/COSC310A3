cfe43ac15c5f1454d77474862d525004380d7e11
ubcbent <bentisserand98@gmail.com>
2019-04-03 14:57:08 -0700

    mine at least works i hope

--- Eclipse Project/src/YeBot.java
+++ Eclipse Project/src/YeBot.java
@@ -1,6 +1,8 @@
 import java.util.*;
 import org.alicebot.ab.*;
 import java.io.File;
+import edu.smu.tspell.wordnet.*;
+import java.util.ArrayList;
 /**
  * YeBot is the main class where the conversation is held.
  */
@@ -16,6 +18,14 @@ public class YeBot {
 		Bot yebot = new Bot("YeBot",dir.substring(0,dir.length()-2));
 		yebot.writeAIMLFiles();
 		String ans;
+		//initialize wordnet, requires that wordnet is installed in the correct dir
+		System.setProperty("wordnet.database.dir", "C:\\Program Files (x86)\\WordNet\\2.1\\dict\\");
+		WordNetDatabase database = WordNetDatabase.getFileInstance();
+		//string array to contain the responses kanye has if he doesn't have a response
+		ArrayList<String> unknown = new ArrayList<String>();
+		unknown.add("wish i could help   i dont know what that means");
+		unknown.add("you got good vibes   but i dont know what to say to that");
+		unknown.add("yo man you gotta slow down   maybe try saying that a different way");
 		
 		Chat session = new Chat(yebot);
 		conversation = new Conversation();
@@ -32,6 +42,7 @@ public class YeBot {
 			if (input!=""&&input!=null&&input.length()>1||i==1) {
 				if(input==""||input==null||input.length()<1) {
 					//start conversation
+					
 					output = conversation.response("Ye is in the BUILDING!");
 					i=0;	
 				}
@@ -39,6 +50,7 @@ public class YeBot {
 					//user calls for exiting the conversation
 					try {
 						Thread.sleep(500);
+						
 						output = conversation.response(session.multisentenceRespond(input));
 						Thread.sleep(1000);
 					} catch (InterruptedException e) {
@@ -48,7 +60,31 @@ public class YeBot {
 				}
 				else {
 					//regular response
-					output = conversation.response(session.multisentenceRespond(input));
+					// process synonyms if kanye has no response !REQUIRES WORDNET TO BE INSTALLED!
+					String response = session.multisentenceRespond(input);
+					if(unknown.contains(response)) {
+						// kanye is confused, typical
+						String[] words = input.split(" ");
+						outerloop:
+						for(int j = 0; j<words.length;j++) {
+							Synset[] syns = database.getSynsets(words[j]);
+							if(syns.length!=0) {
+								for(Synset syn : syns) {
+									String[] replace = syn.getWordForms();
+									for(String r : replace) {
+										String inputnew = input.replaceAll(words[j]+" ", r+" ");
+										response = session.multisentenceRespond(inputnew);
+										if(!unknown.contains(response)) {
+											// if we find a response, break and output
+											input = inputnew;
+											break outerloop;
+										}
+									}
+								}
+							}
+						}
+					}
+					output = conversation.response(response);
 				}
 			}	
 		}
@@ -61,4 +97,5 @@ public class YeBot {
 		//while(ans.toUpperCase().equals("Y"));	//start over if answer is "Y" or "y"
 
 	}
+	
 }
